56a7cdaa2b0ed4207e26c64956e5b8062544bc0e
arseny0709 <arseny.butko771@gmail.com>
2024-09-19 15:39:56 -0700

    cruds for tags

---
+++ pastebin/blog/urls.py
@@ -0,0 +1,33 @@
+
+from django.urls import path
+from . import views
+
+app_name='blog'
+
+urlpatterns = [
+    # main page
+    path('', views.index, name='index'),
+
+    # tags logic
+    path('tags/', views.TagListView.as_view(), name='tag-list'),
+    path('tags/create/', views.TagCreateView.as_view(), name='tag-create'),
+    path('tags/update/<int:pk>/', views.TagUpdateView.as_view(), name='tag-update'),
+    path('tags/delete/<int:pk>/', views.TagDeleteView.as_view(), name='tag-delete'),
+
+
+    # path('categories/', CategoryListView.as_view(), name='category-list'),
+    # path('categories/create/', CategoryCreateView.as_view(), name='category-create'),
+    # path('categories/update/<int:id>', CategoryUpdateView.as_view(), name='category-update'),
+    # path('categories/delete/<int:id>', CategoryDeleteView.as_view(), name='category-delete'),
+    #
+    # path('comments/create/', CommentCreateView.as_view(), name='comment-create'),
+    # path('comments/update/<int:id>', CommentUpdateView.as_view(), name='comment-update'),
+    # path('comments/delete/<int:id>', CommentDeleteView.as_view(), name='comment-delete'),
+    #
+    # path('posts/', PostListView.as_view(), name='post-list'),
+    # path('posts/create/', PostCreateView.as_view(), name='post-create'),
+    # path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # path('posts/update/<int:pk>', PostUpdateView.as_view(), name='post-update'),
+    # path('posts/delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
+
+]
--- pastebin/blog/views.py
+++ pastebin/blog/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404
+from .models import Tag
+from .forms import TagForm
+
+
+# ----------------------------------------------------------------------------
+# main page
+def index(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/index.html', {'tags': tags})
+# ----------------------------------------------------------------------------
+
+
+
+
+
+# ----------------------------------------------------------------------------
+# tags
+class TagListView(ListView):
+    model = Tag
+    template_name = 'tag_list.html'
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = ['name']
+    http_method_names = ['post']  # Разрешаем только POST-запросы
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        tag = self.model.objects.create(name=data['name'])
+        return JsonResponse({'success': True, 'tag': {'id': tag.id, 'name': tag.name}})
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = ['name']
+    http_method_names = ['put']  # Разрешаем только PUT-запросы
+
+    def put(self, request, *args, **kwargs):
+        tag = self.get_object()
+        data = json.loads(request.body)
+        tag.name = data['name']
+        tag.save()
+        return JsonResponse({'success': True, 'tag': {'id': tag.id, 'name': tag.name}})
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    http_method_names = ['delete']  # Разрешаем только DELETE-запросы
+
+    def delete(self, request, *args, **kwargs):
+        tag = self.get_object()
+        tag.delete()
+        return JsonResponse({'success': True})
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+# comments
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+# categories
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+# posts
+# ----------------------------------------------------------------------------
