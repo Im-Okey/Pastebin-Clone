7be33b8a7f7d356eba71700d36eaea2ec18c43d2
Im Okey <arseny.butko771@gmail.com>
2024-10-16 17:53:34 +0300

    cnange ureaded status

--- pastebin/general/urls.py
+++ pastebin/general/urls.py
@@ -6,4 +6,10 @@ app_name = 'general'
 urlpatterns = [
     path('notifications/', views.notifications, name='notifications'),
     path('messages/', views.messages, name='messages'),
+
+    path('notifications/mark-all-read/', views.mark_all_read_notifications, name='mark-all-notes-read'),
+    path('messages/mark-all-read/', views.mark_all_read_messages, name='mark-all-messages-read'),
+
+    path('message/read/<int:message_id>/', views.mark_message_as_read, name='mark-message-read'),
+
 ]
--- pastebin/general/views.py
+++ pastebin/general/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Notifications, Messages
 from blog.models import Paste
@@ -20,10 +20,34 @@ def notifications(request):
 
 def messages(request):
     mess = Messages.objects.all().filter(user=request.user)
+    unread_messages = mess.filter(is_checked=False)
+    read_messages = mess.filter(is_checked=True)
     popular_posts, unread_messages_count, unread_notifications_count = get_general_context(request, Paste)
     return render(request, 'messages.html', {
         'popular_posts': popular_posts,
         'messages': mess,
         'unread_notifications_count': unread_notifications_count,
-        'unread_messages_count': unread_messages_count
+        'unread_messages_count': unread_messages_count,
+        'unread_messages': unread_messages,
+        'read_messages': read_messages
     })
+
+
+def mark_all_read_notifications(request):
+    Notifications.objects.filter(user=request.user, is_checked=False).update(is_checked=True)
+    return redirect('general:notifications')
+
+
+def mark_all_read_messages(request):
+    Messages.objects.filter(user=request.user, is_checked=False).update(is_checked=True)
+    return redirect('general:messages')
+
+
+def mark_message_as_read(request, message_id):
+    message = get_object_or_404(Messages, id=message_id)
+
+    if not message.is_checked:
+        message.is_checked = True
+        message.save()
+
+    return redirect('blog:post-detail', slug=message.post.slug)
