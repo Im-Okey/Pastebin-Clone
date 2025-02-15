cc415eabdda0c72d81e58233ae65f9fb0b57b6ea
Arseny <arseny.butko771@gmail.com>
2025-02-15 16:46:27 +0300

    rebuild docker setup

--- pastebin/staticfiles/js/show_edit_form.js
+++
@@ -1,5 +0,0 @@
-function showEditForm(button) {
-    document.getElementById('post-content-display').style.display = 'none';
-    document.getElementById('edit-post-form').style.display = 'block';
-    button.style.display = 'none';
-}
